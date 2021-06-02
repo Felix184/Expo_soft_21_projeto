b0002b58e502103853b0745d6d8a422331db9bc0
Matheus <matheusfelixcarlos@gmail.com>
2021-06-01 23:18:05 -0300

    adicionando algumas functions no projeto

--- meme generator/js/functions.js
+++ meme generator/js/functions.js
@@ -5,6 +5,64 @@ window.onload = function() {
 function selectImage() {
 	document.getElementById("foto-file").click();
 }
+
+function makeGrayScale() {
+	foto.grayscale();
+}
+
+function makeBright() {
+	foto.makeBright();
+}
+
+function makeDark() {
+	foto.makeDark();
+}
+
+function makeBlur() {
+	foto.applyBlurFilter();
+}
+
+function makeEmboss() {
+	foto.applyEmbossFilter();
+}
+
+function makeSharp() {
+	foto.applySharpFilter();
+}
+
+function openColorpicker() {
+	document.getElementById("color-picker").click();
+}
+
+function makeColorize(elem) {
+	var color = elem.value;
+	foto.colorize(color);
+}
+function openColorFilterPicker() {
+	document.getElementById("colorize-color-picker").click();
+}
+
+function applyColorFilter(elem) {
+	var color = elem.value;
+	foto.applyColorFilter(color);
+}
+
+function makeTransparent() {
+	foto.makeTransparent();
+}
+
+function crop() {
+	foto.cropSelected();
+}
+
+function flipVertically() {
+	foto.flipVertically();
+}
+
+function rotate(elem) {
+	foto.rotate(elem.value);
+}
+
 function download() {
 	foto.export();
 }
