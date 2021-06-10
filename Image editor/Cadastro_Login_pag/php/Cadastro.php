f371157aedd489ded13ce0afe670e6b6b00fb9ca
Hebert <kira01010110@gmail.com>
2021-06-10 18:57:18 -0300

    Atualizando pág Cadastro/Login

---
+++ Image editor/Cadastro_Login_pag/php/Cadastro.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta charset="uft-8"/>
+        <title>Projeto Ligin</title>
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <body>
+        <div id="corpo-form">
+            <h1>Cadastro</h1>
+            <form method="POST" action="processa.php">
+                <input type="email" placeholder="Usuário">
+                <input type="pwd1" placeholder="Senha">
+                <input type="pwd2" placeholder="Confirmação de Senha">
+                <input type="submit" value="ACESSAR">
+                <a href="">Já é inscrito?<strong>Faça seu login!</strong></a>
+            </form>
+        </div>
+    </body>
+</html>
---
+++ Image editor/Cadastro_Login_pag/php/index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="uft-8"/>
+        <title>Projeto Ligin</title>
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <body>
+        <div id="corpo-form">
+            <h1>Entrar</h1>
+            <form method="POST" action="processa.php">
+                <input type="email" placeholder="Usuário">
+                <input type="password" placeholder="Senha">
+                <input type="submit" value="ACESSAR">
+                <a href="">Ainda não é inscrito?<strong>Cadastre-se!</strong></a>
+            </form>
+        </div>
+    </body>
+</html>
